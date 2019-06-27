4e09dcbe6dfcbe4f9ee009ad15e2775c7182356e
Elena Kulakova <kulakovaelena15@gmail.com>
2019-06-27 12:58:25 +0200

    Initial commit

---
+++ myNursingSpots/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewController.swift
+//  myNursingSpots
+//
+//  Created by Darko Kulakov on 2019-06-27.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
